1d03c78ba6a60a23d81f3c5433c980c727b45c22
Glenn McCrea <glennmccrea@icloud.com>
2025-11-04 19:55:10 -0600

    Created Footer.jsx, Linked to CSS, Added to App.jsx

---
+++ react-final-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div id="footer-links">
+        <a href="" class="button-div-footer">
+          About
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
