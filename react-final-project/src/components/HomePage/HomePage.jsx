6840fe17b15d9cc66ac01023df4dd91b44ba5e88
Glenn McCrea <glennmccrea@icloud.com>
2025-11-05 18:00:59 -0600

    created HomPage.jsx and moved the html from my mock-up into it. imported it into App.jsx

---
+++ react-final-project/src/components/HomePage/HomePage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <main>
+      <div id="homepage-div">
+        <section id="homepage-text-container">
+          <h2>Welcome to Homeschool Day Tracker!</h2>
+          <p>
+            We exist to help Teachers track the school day learning and
+            activities in a clean, easy to use log.
+          </p>
+          <br />
+          <h3>
+            To get started, <a href="">Log Now</a>
+          </h3>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
