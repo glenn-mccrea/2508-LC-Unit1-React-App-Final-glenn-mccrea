d682013ef30814dfc7432d27bdd3730dff461164
Glenn McCrea <glennmccrea@icloud.com>
2025-11-05 19:37:00 -0600

    Created AboutPage folder, jsx, and css. wrote simple filler text for the about page.

---
+++ react-final-project/src/components/AboutPage/AboutPage.jsx
@@ -0,0 +1,23 @@
+import "./AboutPage.css";
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <div id="about-page-div">
+        <section id="about-page-text-container">
+          <h2>
+            Homeschool Day Tracker was created to serve teachers like you...
+          </h2>
+          <h3>
+            In 2025 the idea for HSDT was born out of a desire to have a simple,
+            web app that would help Teachers track the school day learning and
+            activities in a clean, easy to use log.
+          </h3>
+        </section>
+      </div>
+    </main>
+  );
+};
+
+export default AboutPage;
