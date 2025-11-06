d6e4bd7b408d44c8d7f32897e31d472d79c49d3a
Glenn McCrea <glennmccrea@icloud.com>
2025-11-05 19:48:32 -0600

    created about page. modified and adapted css from home page for a matching look.

--- react-final-project/src/components/AboutPage/AboutPage.jsx
+++ react-final-project/src/components/AboutPage/AboutPage.jsx
@@ -1,18 +1,19 @@
 import "./AboutPage.css";
 import React from "react";
+import background from "./about-page-background.jpg";
 
 const AboutPage = () => {
   return (
     <main>
-      <div id="about-page-div">
-        <section id="about-page-text-container">
+      <div id="aboutpage-div" style={{ backgroundImage: `url(${background})` }}>
+        <section id="aboutpage-text-container">
           <h2>
             Homeschool Day Tracker was created to serve teachers like you...
           </h2>
           <h3>
-            In 2025 the idea for HSDT was born out of a desire to have a simple,
-            web app that would help Teachers track the school day learning and
-            activities in a clean, easy to use log.
+            In 2025 the idea for HSDT was born out of a desire to have a simple
+            web app that would help Teachers track school day learning and
+            activities in a clean, easy to use way.
           </h3>
         </section>
       </div>
