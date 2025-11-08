b22b86fcd4d886025d20999112c6c20bde27fc07
Glenn McCrea <glennmccrea@icloud.com>
2025-11-08 10:07:16 -0600

    added html and CSS, linked them, and made the first responsive breakpoint for the tablet/mobile view.

--- react-final-project/src/components/ViewerPage/ViewerPage.jsx
+++ react-final-project/src/components/ViewerPage/ViewerPage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./viewer-page.css";
+import LogCard from "../LogCard/LogCard";
 
 const ViewerPage = () => {
   return (
     <main>
       <div id="viewer-page-div">
-        <div id="card-holder-container">CARD GOES HERE</div>
+        <div id="card-holder-container">
+          <LogCard />
+        </div>
       </div>
     </main>
   );
