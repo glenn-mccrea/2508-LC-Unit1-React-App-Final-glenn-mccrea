5bf91b4587af84b33e795d69f7895f5e6e82b74c
Glenn McCrea <glennmccrea@icloud.com>
2025-11-08 19:49:30 -0600

    useState used to set what displays

--- react-final-project/src/components/LoadingPage/LoadingPage.jsx
+++ react-final-project/src/components/LoadingPage/LoadingPage.jsx
@@ -1,20 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./loading-page.css";
 
 const LoadingPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
   return (
     <div id="loading-page-main-div">
-      <div id="loading-statement">
-        <p>
-          <em>loading...</em>
-        </p>
+      {isLoading ? (
+        <div id="loading-statement">
+          <p>
+            <em>loading...</em>
+          </p>
 
-        <p className="loader"></p>
-      </div>
-
-      <div id="no-logs-statement">
-        <p>Sorry, no log items to see here.</p>
-      </div>
+          <p className="loader"></p>
+        </div>
+      ) : (
+        <div id="no-logs-statement">
+          <p>Sorry, no log items to see here.</p>
+        </div>
+      )}
     </div>
   );
 };
