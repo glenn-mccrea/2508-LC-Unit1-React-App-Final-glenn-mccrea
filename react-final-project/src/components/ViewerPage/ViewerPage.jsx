3e38d03ec672e9a3f11bf3d49f6bfb4a1196d1ae
Glenn McCrea <glennmccrea@icloud.com>
2025-11-10 19:55:00 -0600

    Built the structure to iterate over the array, but something is not working. Getting a type error.

--- react-final-project/src/components/ViewerPage/ViewerPage.jsx
+++ react-final-project/src/components/ViewerPage/ViewerPage.jsx
@@ -8,7 +8,16 @@ const ViewerPage = ({ cards }) => {
     <main>
       <div id="viewer-page-div">
         <div id="card-holder-container">
-          <LogCard />
+          {/* Below: map loops through the array as many times as there are contents. Everytime it does run, it pulls out the data and places it onto a LogCard. */}
+          {cards.map((card) => (
+            <LogCard
+              key={card.id}
+              subject={card.subject}
+              duration={card.duration}
+              materials={card.materials}
+              notes={card.notes}
+            />
+          ))}
         </div>
       </div>
     </main>
